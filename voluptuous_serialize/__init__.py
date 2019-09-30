1d5c6256cf2f2b87717592844656e9eb372b9f02
emontnemery <erik@montnemery.com>
2019-09-30 22:50:20 +0200

    Add support for custom serializer

--- voluptuous_serialize/__init__.py
+++ voluptuous_serialize/__init__.py
@@ -12,11 +12,16 @@ TYPES_MAP = {
 }
 
 
-def convert(schema):
+def convert(schema, custom_serializer=None):
     """Convert a voluptuous schema to a dictionary."""
     # pylint: disable=too-many-return-statements,too-many-branches
     if isinstance(schema, vol.Schema):
         schema = schema.schema
+
+    if custom_serializer:
+        val = custom_serializer(schema)
+        if val:
+            return val
 
     if isinstance(schema, Mapping):
         val = []
@@ -29,7 +34,7 @@ def convert(schema):
             else:
                 pkey = key
 
-            pval = convert(value)
+            pval = convert(value, custom_serializer)
             pval['name'] = pkey
             if description is not None:
                 pval['description'] = description
@@ -47,7 +52,7 @@ def convert(schema):
     if isinstance(schema, vol.All):
         val = {}
         for validator in schema.validators:
-            val.update(convert(validator))
+            val.update(convert(validator, custom_serializer))
         return val
 
     if isinstance(schema, (vol.Clamp, vol.Range)):
