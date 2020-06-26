8c063358de3f2b6d0c51cd20bae2e483a213c43f
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2020-06-25 22:43:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='voluptuous-serialize',
-      version='2.3.0',
+      version='2.4.0',
       description='Convert voluptuous schemas to dictionaries',
       url='http://github.com/balloob/voluptuous-serialize',
       author='Paulus Schoutsen',
