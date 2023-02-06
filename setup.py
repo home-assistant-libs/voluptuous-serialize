c6875103c38f1db09bc14d100a1eeb5ead8bc62e
Martin Hjelmare <marhje52@gmail.com>
2023-02-06 09:41:47 +0100

    Run black

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
-setup(name='voluptuous-serialize',
-      version='2.5.0',
-      description='Convert voluptuous schemas to dictionaries',
-      url='http://github.com/balloob/voluptuous-serialize',
-      author='Paulus Schoutsen',
-      author_email='Paulus@PaulusSchoutsen.nl',
-      license='Apache License 2.0',
-      install_requires=['voluptuous'],
-      packages=['voluptuous_serialize'],
-      zip_safe=True)
+setup(
+    name="voluptuous-serialize",
+    version="2.5.0",
+    description="Convert voluptuous schemas to dictionaries",
+    url="http://github.com/balloob/voluptuous-serialize",
+    author="Paulus Schoutsen",
+    author_email="Paulus@PaulusSchoutsen.nl",
+    license="Apache License 2.0",
+    install_requires=["voluptuous"],
+    packages=["voluptuous_serialize"],
+    zip_safe=True,
+)
